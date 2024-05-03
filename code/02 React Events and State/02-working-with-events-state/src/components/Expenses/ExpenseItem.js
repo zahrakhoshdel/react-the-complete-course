18e4605113179bb745e1dcb2cd869ab037c4a1b5
unknown <imzahrakhoshdel@gmail.com>
2024-05-03 22:01:57 +0330

    listening to Events & working with Event Handlers

---
+++ code/02 React Events and State/02-working-with-events-state/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
