a8c830ce18ef6c51be96e1eee3877735cc9fd499
unknown <imzahrakhoshdel@gmail.com>
2024-05-04 22:22:20 +0330

    working with Lists

---
+++ code/03 Rendering Lists - Conditional Content/src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onDateFilter={filterChangeHandler}
+      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
