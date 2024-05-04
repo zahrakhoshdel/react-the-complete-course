8863fca960ceab3e0c34b05fef0a1eb80823b485
unknown <imzahrakhoshdel@gmail.com>
2024-05-04 13:34:52 +0330

    handling form submission and working with multiple States

--- code/02 React Events and State/02-working-with-events-state/src/components/NewExpense/ExpenseForm.js
+++ code/02 React Events and State/02-working-with-events-state/src/components/NewExpense/ExpenseForm.js
@@ -2,35 +2,37 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  //workin with multiple state
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (event) => {
-    setUserInput({
-        ...userInput,
-        enteredTitle: event.target.value,
-    })
+    setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    setUserInput({
-        ...userInput,
-        enteredAmount: event.target.value,
-    })
+    setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    setUserInput({
-        ...userInput,
-        enteredDate: event.target.value,
-    })
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
