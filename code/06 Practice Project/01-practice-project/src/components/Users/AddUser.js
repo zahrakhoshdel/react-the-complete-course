cc16a8468bd435d4c3f812ed84d5121f53928c82
unknown <imzahrakhoshdel@gmail.com>
2024-05-14 00:24:42 +0330

    adding a re-usable Button component

--- code/06 Practice Project/01-practice-project/src/components/Users/AddUser.js
+++ code/06 Practice Project/01-practice-project/src/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import Card from "../UI/Card";
-import classes from './AddUser.module.css';
+import Button from "../UI/Button";
+
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -7,13 +9,13 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className = {classes.input} >
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
