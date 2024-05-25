a55637e74d9d9ca88e9f159ce4ae9abb4cd972f0
unknown <imzahrakhoshdel@gmail.com>
2024-05-25 22:57:20 +0330

    making context dynamic

--- code/08 Side Effects/04-using-the-react-context-api/src/components/MainHeader/Navigation.js
+++ code/08 Side Effects/04-using-the-react-context-api/src/components/MainHeader/Navigation.js
@@ -1,9 +1,9 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../../context/auth-context";
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
 
   return (
@@ -21,7 +21,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
