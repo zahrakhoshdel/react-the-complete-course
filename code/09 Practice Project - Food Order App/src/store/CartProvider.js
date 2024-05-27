0aab63de6092ee31de1d61f537054f7ba1729c65
unknown <imzahrakhoshdel@gmail.com>
2024-05-27 20:11:15 +0330

    working with Refs & forward refs - adding amount to cart

--- code/09 Practice Project - Food Order App/src/store/CartProvider.js
+++ code/09 Practice Project - Food Order App/src/store/CartProvider.js
@@ -40,6 +40,7 @@ const CartProvider = (props) => {
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+  
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
