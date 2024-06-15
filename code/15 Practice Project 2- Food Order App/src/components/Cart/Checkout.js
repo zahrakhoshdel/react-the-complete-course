2694b8edab3e443a55e3ed24f5d3f19f4bdafa01
unknown <imzahrakhoshdel@gmail.com>
2024-06-15 12:54:45 +0330

    submitting & sending cart data

--- code/15 Practice Project 2- Food Order App/src/components/Cart/Checkout.js
+++ code/15 Practice Project 2- Food Order App/src/components/Cart/Checkout.js
@@ -43,6 +43,17 @@ const Checkout = (props) => {
       enteredStreetIsValid &&
       enteredCityIsValid &&
       enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode,
+    });
   };
 
   const nameControlClasses = `${classes.control} ${
