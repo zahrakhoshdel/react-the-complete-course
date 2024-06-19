9d5458ff6b45c32d95b55d6294fd8eae095288d6
unknown <imzahrakhoshdel@gmail.com>
2024-06-19 21:56:06 +0330

    finalizing the fetching logic

--- code/17 Advanced Redux/04-using-an-action-creator-thunk/src/store/cart-actions.js
+++ code/17 Advanced Redux/04-using-an-action-creator-thunk/src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -47,7 +52,10 @@ export const sendCartData = (cart) => {
         "https://react-app-6151c-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
