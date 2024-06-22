115cda345230271181fba9f33d4f436d64c9aeaf
unknown <imzahrakhoshdel@gmail.com>
2024-06-22 19:49:33 +0330

    working with links

--- code/18 Building a Multi-Page SPA with React Router/02-building-multi-page-SPA/src/App.js
+++ code/18 Building a Multi-Page SPA with React Router/02-building-multi-page-SPA/src/App.js
@@ -2,13 +2,19 @@ import { Routes, Route } from "react-router-dom";
 
 import Welcome from "./pages/welcome";
 import Products from "./pages/products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/welcome" element={<Welcome/>}/>
-      <Route path="/products" element={<Products/>}/>
-    </Routes>
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- code/18 Building a Multi-Page SPA with React Router/02-building-multi-page-SPA/src/pages/welcome.js
+++ code/18 Building a Multi-Page SPA with React Router/02-building-multi-page-SPA/src/pages/welcome.js
@@ -1,5 +1,5 @@
-const Welcome = ()=>{
-    return <h1>The Welcome Page</h1>
+const Welcome = () => {
+  return <h1>The Welcome Page</h1>;
 };
 
 export default Welcome;
