d08c725733c5e3286e555d9fa68862e244905372
unknown <imzahrakhoshdel@gmail.com>
2024-06-22 20:27:07 +0330

    adding dynamic routes with params

--- code/18 Building a Multi-Page SPA with React Router/02-building-multi-page-SPA/src/App.js
+++ code/18 Building a Multi-Page SPA with React Router/02-building-multi-page-SPA/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Welcome from "./pages/welcome";
 import Products from "./pages/products";
+import ProductDetail from "./pages/ProductDetail";
 import MainHeader from "./components/MainHeader";
 
 function App() {
@@ -12,6 +13,10 @@ function App() {
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/products" element={<Products />} />
+          <Route
+            path="/product-detail/:productId"
+            element={<ProductDetail />}
+          />
         </Routes>
       </main>
     </div>
