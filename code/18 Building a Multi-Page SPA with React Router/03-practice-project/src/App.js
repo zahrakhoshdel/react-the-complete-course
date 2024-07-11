778bfb286967be1c73b675dec58324169d07c3eb
unknown <imzahrakhoshdel@gmail.com>
2024-07-11 18:42:30 +0330

    redirecting & extracting params

--- code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/App.js
+++ code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -8,6 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/quotes" replace />} />
         <Route path="/quotes" element={<AllQuotes />} />
         <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
         <Route path="/new-quote" element={<NewQuote />} />
