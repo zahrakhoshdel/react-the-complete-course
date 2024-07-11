18b2a84ba83951c49392646107e5a5269d329552
unknown <imzahrakhoshdel@gmail.com>
2024-07-11 18:11:41 +0330

    defining and using routes

--- code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/App.js
+++ code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/quotes" element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
+        <Route path="/new-quote" element={<NewQuote />} />
+      </Routes>
+    </Router>
   );
 }
 
