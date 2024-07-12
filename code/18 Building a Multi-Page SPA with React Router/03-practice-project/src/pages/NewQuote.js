7bb8278f3e1dc42dd553d907b3cc456ed53da732
unknown <imzahrakhoshdel@gmail.com>
2024-07-12 19:13:30 +0330

    implementing programmatic (imperative) navigation

--- code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/pages/NewQuote.js
+++ code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const navigate = useNavigate();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    navigate("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
