f582211d143355cd388ee92a77bc823ae8c330cb
unknown <imzahrakhoshdel@gmail.com>
2024-07-12 13:49:04 +0330

    outputing data on the details page

--- code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/pages/QuoteDetail.js
+++ code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/pages/QuoteDetail.js
@@ -1,13 +1,25 @@
 import { Fragment } from "react";
 import { useParams, Outlet } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Zahra", text: "Learning React is fun!" },
+  { id: "q2", author: "Max", text: "Learning React is great!" },
+];
+
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page </h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Outlet />
     </Fragment>
   );
