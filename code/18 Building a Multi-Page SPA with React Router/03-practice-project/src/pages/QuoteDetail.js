224d4fd612f7051eadccd791eaf49cdf1eec36ca
unknown <imzahrakhoshdel@gmail.com>
2024-07-12 22:08:29 +0330

    getting creative with nested routes

--- code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/pages/QuoteDetail.js
+++ code/18 Building a Multi-Page SPA with React Router/03-practice-project/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Outlet } from "react-router-dom";
+import { useParams, Outlet, Link, useResolvedPath } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -10,6 +10,9 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
+  const path = useResolvedPath();
+
+  const showComments = path.pathname === `/quotes/${params.quoteId}`;
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -20,6 +23,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      {showComments && (
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      )}
+
       <Outlet />
     </Fragment>
   );
