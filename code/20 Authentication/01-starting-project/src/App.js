17ab650e9aa1cd0217c68a723c06131e4c90bde2
unknown <imzahrakhoshdel@gmail.com>
2024-07-16 22:43:35 +0330

    starting project - authentication

---
+++ code/20 Authentication/01-starting-project/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
