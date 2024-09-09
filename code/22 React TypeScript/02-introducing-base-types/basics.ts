46fdfc4766f0c38ef611dddbf51b87d5b13cbce6
unknown <imzahrakhoshdel@gmail.com>
2024-09-09 15:56:13 +0330

    working with array & object types

---
+++ code/22 React TypeScript/02-introducing-base-types/basics.ts
@@ -0,0 +1,38 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 30;
+
+let userName: string;
+
+userName = "Zahra";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Zahra",
+  age: 30,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
