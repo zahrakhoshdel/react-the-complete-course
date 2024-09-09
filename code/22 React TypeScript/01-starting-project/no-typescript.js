50820593843a1a4833dd748f3bf3d3740e2cece6
unknown <imzahrakhoshdel@gmail.com>
2024-09-09 14:26:37 +0330

    starting project - example with javascript

---
+++ code/22 React TypeScript/01-starting-project/no-typescript.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  return a + b;
+}
+
+const resultNumber = add(2, 5); // result = 7
+const resultString = add("2", "5"); // result = 25
+
+console.log(resultNumber);
+console.log(resultString);
